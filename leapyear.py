0acbc433a4b78351af0007425583e21e02aa6329
dillivarmaaws <dillivarmaaws@gmail.com>
2022-09-13 15:33:59 +0530

    Added header. Fixed missing double quotes

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,12 @@
-year=int(input("Enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
-    print("The year is a leap year!)
+# 
+# This is a python program to find if a given year is leap year or not
+#
+# AUTHOR : H Dilli. 2022
+#
+
+year = int( input("Enter year to be checked:"))
+if( year%4 == 0 and year%100 != 0 or year%400 == 0):
+    print("The year is a leap year!")
 else:
-    print("The year isn't a leap year!)
+    print("The year is not a leap year!")
 
